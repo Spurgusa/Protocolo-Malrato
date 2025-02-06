844753e6ddbb6159dafd1f1511dd014feec51e55
spurgus <dpanos@micap.es>
2025-02-06 20:32:36 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+ import React from 'react'
+import ReactDOM from 'react-dom/client'
+import IncidentReportApp from './components/incidente-app'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <IncidentReportApp />
+  </React.StrictMode>,
+)

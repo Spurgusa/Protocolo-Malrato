1c899fcbf2e999d73638423f974eae2749204b20
spurgus <dpanos@micap.es>
2025-02-06 20:54:21 +0100

    Fix import extension in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
- import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import IncidentReportApp from './components/incidente-app'
+import IncidentReportApp from './components/incidente-app.tsx'  // Añadimos .tsx
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
